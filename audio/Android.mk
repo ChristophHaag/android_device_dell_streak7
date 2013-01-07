5155a47dfc1865367d6afb0fcf96504f68e402d9
Giveen <ajaxx20020@gmail.com>
2013-01-07 12:42:42 -0700

    Update audio/Android.mk

--- audio/Android.mk
+++ audio/Android.mk
@@ -20,9 +20,9 @@ LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := audio_hw.c
 LOCAL_C_INCLUDES += \
-external/tinyalsa/include \
-system/media/audio_utils/include \
-system/media/audio_effects/include
+  external/tinyalsa/include \
+  system/media/audio_utils/include \
+  system/media/audio_effects/include
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
 LOCAL_MODULE_TAGS := optional
 
