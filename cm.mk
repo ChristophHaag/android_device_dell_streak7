6b24b4fe9ec6f7450957dc68d90d3d24aa954c6b
Giveen <ajaxx20020@gmail.com>
2013-07-17 12:05:11 -0600

    Dont need this twice

--- cm.mk
+++ cm.mk
@@ -5,10 +5,3 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-PRODUCT_NAME := cm_streak7
-PRODUCT_RELEASE_NAME := Streak7
-PRODUCT_DEVICE := streak7
-PRODUCT_BRAND := Dell
-PRODUCT_MODEL := Streak 7
-PRODUCT_MANUFACTURER := Dell Inc.
