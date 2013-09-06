cbc1dd9b0f747520f6cf64061884d8f227f74912
Giveen <ajaxx20020@gmail.com>
2013-09-06 12:45:53 -0600

    GSM needs permissions

--- streak7.mk
+++ streak7.mk
@@ -25,7 +25,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
     
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/prebuilts/etc/vold.fstab:system/etc/vold.fstab \
