aa7e634cdaceb85e51b4b4f38d37cd00a8e27adf
Giveen <ajaxx20020@gmail.com>
2013-11-08 13:59:02 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo aokp_streak7-userdebug
-cp device/dell/streak7/streak7-product-makefile vendor/aokp/products/streak7.mk
+add_lunch_combo cm_streak7-userdebug
